74c49896ece51a81612005a9e8af7f0308f04018
ashish <ashish@ashishs-air>
2024-08-13 10:56:23 +0530

    created by anand

---
+++ Biswagithub/src/test/java/LeadTest/ToCreateLead.java
@@ -0,0 +1,9 @@
+package LeadTest;
+
+public class ToCreateLead {
+
+	public static void main(String[] args) {
+		System.out.println("lead created by Anand");
+	}
+
+}
--- Biswagithub/src/test/java/contactTest/toCreateContact.java
+++ Biswagithub/src/test/java/contactTest/toCreateContact.java
@@ -4,6 +4,7 @@ public class toCreateContact {
 
 	public static void main(String[] args) {
 		System.out.println("contact created by Biswa");
+		System.out.println("contact created by Anand");
 	}
 
 }
